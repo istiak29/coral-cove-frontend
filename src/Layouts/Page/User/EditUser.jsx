be638321f1e4cc398994c8cb69fcbe02dd8ecfd9
Istiak Islam <tistiak@outlook.com>
2023-12-09 02:31:06 +0600

    edit user info and update context

--- src/Layouts/Page/User/EditUser.jsx
+++ src/Layouts/Page/User/EditUser.jsx
@@ -1,13 +1,20 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { userContext } from "../../../UseContext/ContextProvider";
 
 const EditUser = () => {
+
+    const {  setUserDetails } = useContext(userContext);
+
 
     const { id } = useParams();
 
     const [names, setNames] = useState('');
     const [emails, setEmails] = useState('')
+
+    const navigate = useNavigate()
+    
 
     useEffect(() => {
         axios.get('http://localhost:5000/edit/' + id)
@@ -19,7 +26,21 @@ const EditUser = () => {
             .catch(error => {
                 console.log(error)
             })
-    }, [])
+    }, [id])
+
+    const handleUpdate = (e) => {
+        e.preventDefault()
+
+        axios.put('http://localhost:5000/update/' + id, { names, emails })
+            .then(res => {
+                console.log(res)
+                setUserDetails({name: names, email: emails})
+                navigate('/profile')
+            })
+            .catch(error => {
+            console.log(error)
+        })
+    }
 
     return (
         <div>
@@ -28,23 +49,25 @@ const EditUser = () => {
                 <div className="hero-content text-center text-neutral-content">
                     <div className="max-w-md">
                         <h1 className="mb-5 text-5xl font-bold text-zinc-200">Update Your Info!</h1>
-                        <div className="mb-5">
-                            {/* ---Edit area */}
-                            <div className="">
-                                <h2>Your Name: { names}</h2>
-                                <input type="email" name="email" placeholder="Type here" className="mt-2 input input-bordered input-success w-full max-w-xs" />
-                            </div>
+                        <form onSubmit={handleUpdate}>
+                            <div className="mb-5">
+                                {/* ---Edit area */}
+                                <div className="">
+                                    <h2>Your Name: {names}</h2>
+                                    <input type="text" name="name" placeholder="Type here" className="mt-2 input input-bordered input-success w-full max-w-xs" value={names} onChange={(e) => setNames(e.target.value)} />
+                                </div>
 
-                            {/* ---Edit area */}
-                            <div className="mt-5">
-                                <h2>Your Email: { emails}</h2>
-                                <input type="email" name="email" placeholder="Type here" className="mt-2 input input-bordered input-success w-full max-w-xs" />
-                            </div>
+                                {/* ---Edit area */}
+                                <div className="mt-5">
+                                    <h2>Your Email: {emails}</h2>
+                                    <input type="email" name="email" placeholder="Type here" className="mt-2 input input-bordered input-success w-full max-w-xs" value={emails} onChange={(e) => setEmails(e.target.value)} />
+                                </div>
 
-                            <div>
-                                <button type="submit" className="btn btn-success mt-5 text-white">Save Change</button>
+                                <div>
+                                    <button type="submit" className="btn btn-success mt-5 text-white">Save Change</button>
+                                </div>
                             </div>
-                        </div>
+                        </form>
 
                     </div>
                 </div>
